e7b2e7e98afee1e4f861580810c4490dd1f8fdd1
Zachary Boehm <zboehm@menard-inc.com>
2023-02-23 14:47:55 -0600

    Update package format

--- TODO.md
+++ TODO.md
@@ -1,5 +1,9 @@
 # TODO
 
-- [ ] Add logic for serving custom error pages from sub directory
+- [x] Add logic for serving custom error pages from sub directory
+- [x] Test watch directories
 - [ ] Add better logic around custom callbacks
-- [ ] Test watch directories
+  - [x] Custom pattern match for updating keys
+  - [x] Callbacks must have the signature `(root: str, src: str) -> tuple[bool, str|None, str|None]`. The callbacks must take the servers root and the path of the source file that has been updated. Callbacks must return the state to set the key(s) to, the exact key to update and pattern of additional keys to update.
+- [x] Custom server path object that handles all need logic including isdir and isfile.
+- [ ] Dynamic caching header add or remove
--- liveserver/__main__.py
+++ liveserver/__main__.py
@@ -6,35 +6,51 @@ import click
 
 from liveserver import __version__, LiveServer
 
-@click.option("-r", "--root", default="", help="path where the server should attach")
-@click.option(
-    "-b",
-    "--base",
-    default="",
-    help="base path where the server will find custom error files."
-)
-@click.option("-p", "--port", default=3031, help="port to serve to")
-@click.option(
-    "-w",
-    "--watch",
-    multiple=True,
-    default=[],
-    help="list of paths to watch for changes. This must be in the format of a python list with \
-only strings inside. Ex. ['blog/python/']"
-)
-@click.option("-v", "--version", flag_value=True, help="Version of mophidian", default=False)
+HELP = {
+    "root": "path where the server should attach",
+    "base": "base path where the server will find custom error files.",
+    "serve": "port that the server will serve to",
+    "watch": "list of paths to watch. Repeat the command for each entry to the list",
+    "version": "Version of mophidian",
+    "silent": "Surpress all logs from the server and file watcher",
+    "open": "Toggle on auto open. This will automatically open the base url in the browser."
+}
+
+@click.option("-r", "--root", default="", help=HELP["root"])
+@click.option("-b", "--base", default="", help=HELP["base"])
+@click.option("-p", "--port", default=3031, help=HELP["serve"])
+@click.option("-w", "--watch", multiple=True, default=[], help=HELP["watch"])
+@click.option("-v", "--version", flag_value=True, default=False, help=HELP["version"])
+@click.option("-o", "--open", flag_value=True, default=False, help=HELP["open"])
+@click.option("-s", "--silent", flag_value=True, default=False, help=HELP["silent"])
 @click.command(name="serve")
-def serve(root: str="", base: str = "", port: int=3031, watch: list[str]=[], version: bool = False):
-    """Serve a specific path or the cwd by default. Watch for updates in files in the paths provided
-    or in cwd by default. If changes are found then call the appropriate callback and reload the page
-    if the callback returns True.
+def serve(
+    root: str="",
+    base: str = "",
+    port: int=3031,
+    watch: list[str]=[],
+    version: bool = False,
+    silent: bool = False,
+    open: bool = False,
+):
+    """ Serve a specific path or the cwd by default. Watch for updates in files in the paths
+    provided or in cwd by default. If changes are found then call the appropriate callback and
+    reload the page if the callback returns True.
     """
 
     if version:
         click.echo(f"Livereload v{__version__}")
         exit()
 
-    liveserver = LiveServer(*watch, root=root, base=base, port=port)
+    liveserver = LiveServer(
+        *watch,
+        root=root,
+        base=base,
+        port=port,
+        surpress=silent,
+        auto_open=open
+    )
+
     try:
         print(f"Started serving at http://localhost:{port}/")
         liveserver.start()
